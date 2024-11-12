a3ba5a7f362b3ec3d0ba81e11ea2e66c35edf3a8
Erik Schierboom <erik_schierboom@hotmail.com>
2024-11-12 10:09:09 +0100

    Initial commit (#1)

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
-FROM alpine:3.18
+FROM rust:1.82.0-alpine3.20 AS builder
 
-# install packages required to run the tests
-RUN apk add --no-cache jq coreutils
+RUN apk add --no-cache linux-headers make musl-dev
+
+RUN cargo install uiua@0.13.0
+
+FROM alpine:3.20
+
+RUN apk add --no-cache jq
+
+COPY --from=builder /usr/local/cargo/bin/uiua /usr/local/bin
 
 WORKDIR /opt/test-runner
 COPY . .
--- bin/run.sh
+++ bin/run.sh
@@ -15,8 +15,7 @@
 # Example:
 # ./bin/run.sh two-fer path/to/solution/folder/ path/to/output/directory/
 
-# If any required arguments is missing, print the usage and exit
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+if [ $# -lt 3 ]; then
     echo "usage: ./bin/run.sh exercise-slug path/to/solution/folder/ path/to/output/directory/"
     exit 1
 fi
@@ -26,35 +25,22 @@ solution_dir=$(realpath "${2%/}")
 output_dir=$(realpath "${3%/}")
 results_file="${output_dir}/results.json"
 
-# Create the output directory if it doesn't exist
 mkdir -p "${output_dir}"
 
 echo "${slug}: testing..."
 
-# Run the tests for the provided implementation file and redirect stdout and
-# stderr to capture it
-test_output=$(false)
-# TODO: substitute "false" with the actual command to run the test:
-# test_output=$(command_to_run_tests 2>&1)
+test_output=$(uiua test "${solution_dir}/tests.ua")
 
-# Write the results.json file based on the exit code of the command that was 
-# just executed that tested the implementation file
 if [ $? -eq 0 ]; then
     jq -n '{version: 1, status: "pass"}' > ${results_file}
 else
-    # OPTIONAL: Sanitize the output
-    # In some cases, the test output might be overly verbose, in which case stripping
-    # the unneeded information can be very helpful to the student
-    # sanitized_test_output=$(printf "${test_output}" | sed -n '/Test results:/,$p')
+    if printf "${test_output}" | grep -q -E "tests? (failed|passed)"; then
+        status="fail"
+    else
+        status="error"
+    fi
 
-    # OPTIONAL: Manually add colors to the output to help scanning the output for errors
-    # If the test output does not contain colors to help identify failing (or passing)
-    # tests, it can be helpful to manually add colors to the output
-    # colorized_test_output=$(echo "${test_output}" \
-    #      | GREP_COLOR='01;31' grep --color=always -E -e '^(ERROR:.*|.*failed)$|$' \
-    #      | GREP_COLOR='01;32' grep --color=always -E -e '^.*passed$|$')
-
-    jq -n --arg output "${test_output}" '{version: 1, status: "fail", message: $output}' > ${results_file}
+    jq -n --arg output "${test_output}" --arg status "${status}" '{version: 1, status: $status, message: $output}' > "${results_file}"
 fi
 
 echo "${slug}: done"
